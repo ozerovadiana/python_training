e0b3672cfc736752dd3fb4355801b26dba70be5d
Diana <ozerovadianaa@gmail.com>
2021-06-18 15:16:06 +0300

    3 домашнее задание.

---
+++ contact.py
@@ -0,0 +1,30 @@
+class contact_info_bday :
+    def __init__(self, bday_day, bday_month,  bday_year):
+        self.bday_day = bday_day
+        self. bday_month=bday_month
+        self.bday_year=bday_year
+
+class  contact_info_name :
+    def __init__(self, first_name, middle_name, last_name):
+        self.first_name =first_name
+        self.middle_name=middle_name
+        self.last_name=last_name
+
+class  contact_phones :
+    def __init__(self, mobile, home_phone, work_phone):
+        self.mobile = mobile
+        self.home_phone = home_phone
+        self.work_phone = work_phone
+
+class  contact_emails :
+    def __init__(self, email, email2, email3):
+        self.email = email
+        self.email2 = email2
+        self.email3 = email3
+
+class  contact_secondary :
+    def __init__(self, address2, phone2, notes):
+        self.address2 = address2
+        self.phone2 = phone2
+        self.notes = notes
+
