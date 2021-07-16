efddf76400ed804b741c6bbdc71d01f66904b01d
Diana <ozerovadianaa@gmail.com>
2021-07-16 10:13:51 +0300

    4 домашнее задание.правки

--- test_add_contact.py
+++ test_add_contact.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 from selenium import webdriver
 import pytest
-from application_contact import Application
+from application import Application
 from contact import *
 
 @pytest.fixture
@@ -13,7 +13,7 @@ def app(request):
 
 def test_add_contact(app):
     app.open_home_page()
-    app.log_in("admin", "secret")
+    app.login("admin", "secret")
     app.fill_name(contact_info_name(first_name="diana", middle_name="olegovna", last_name="ozerova"))
     app.fill_nickname("ozerovadiana")
     app.fill_title("qwerty")
@@ -23,5 +23,5 @@ def test_add_contact(app):
     app.fill_emails(contact_emails(email="sss@ss.ru", email2="sssss@ssss.ru", email3="qwer@fr.ru"))
     app.fill_bday(contact_info_bday(bday_day="7", bday_month="May", bday_year="1996"))
     app.fill_secondary(contact_secondary(address2="russia,moscow", phone2="1", notes="wwww"))
-    app.log_out()
+    app.logout()
 
